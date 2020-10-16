564e9688959d0ed8a6f0398784de8440ad1fd3a9
Michel Castilho <devmichelcastilho@gmail.com>
2020-10-16 14:37:43 -0300

    Instalacao sqlite3
    Requisicoes basicas montadas em server.ts

--- nextlevelweek3/web/backend/src/server.ts
+++ nextlevelweek3/web/backend/src/server.ts
@@ -2,4 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+//app.get('/users', () => {
+//   console.log('teste');
+//});
+
+app.post('/users/:id', (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
+    //return response.json( { message: 'teste' } );
+});
+
 app.listen(3333);
