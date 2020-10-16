9451924d16ba09f05703684df629f84b5ed0e03e
Michel Castilho <devmichelcastilho@gmail.com>
2020-10-16 18:02:34 -0300

    Cadastro no BD funcionando com requisicao manual

--- nextlevelweek3/web/backend/src/server.ts
+++ nextlevelweek3/web/backend/src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 
+import { getRepository  } from 'typeorm';
+
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
@@ -8,11 +14,42 @@ app.use(express.json());
 //   console.log('teste');
 //});
 
-app.post('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
+//app.post('/users/:id', (request, response) => {
+    //console.log(request.query);
+    //console.log(request.params);
+    //console.log(request.body);
     //return response.json( { message: 'teste' } );
+//});
+
+app.post('/orphanages', async (request, response) => { // USAR "async" SEMPRE QUE PRECISAR USAR AWAIT. QUERYS SÃO ASSÍNCRONAS!!
+
+    console.log(request.body);
+
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const OrphanagesRepository = getRepository(Orphanage);
+
+    const orphanage =  OrphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await OrphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage); // STATUS 201 SIGNIFICA QUE A CRIAÇÃO FUNCIONOU
 });
 
 app.listen(3333);
